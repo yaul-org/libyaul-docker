87addb14a8e8c8ff6e139ecc0c6c51d29cb044c3
Israel Jacquez <mrkotfw@gmail.com>
2019-10-09 18:11:39 -0700

    Update tool-chain and dependencies

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,8 +4,8 @@ WGET_OPTIONS="--retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -
 
 GIT_URL="https://github.com/ijacquez/libyaul.git"
 
-TOOLCHAIN_URL="https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1j1O31Lzl2uyl2n9Hn2iFg92bHQB_vN3R"
-TOOLCHAIN_FILE="tool-chain-MinGW-x86_64-20180609.7z"
+TOOLCHAIN_URL="https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1YanSEu9gLcnttQwowUyZhXzHNvHyY1wT"
+TOOLCHAIN_FILE="tool-chain-MinGW-x86_64-20191007.tar"
 
 change_env_value() {
     local _variable="${1}"
@@ -27,16 +27,19 @@ cd /opt
 
 /usr/bin/rm -r -f libyaul
 
-/usr/bin/pacman -S --noconfirm git make gcc wget unzip zip p7zip diffutils dos2unix patch tar
+/usr/bin/pacman -S --noconfirm git make gcc wget unzip zip p7zip diffutils dos2unix patch tar python3 python3-pip
 
 /usr/bin/sync
 /usr/bin/sync
 /usr/bin/sync
+
+# Install other dependencies
+pip3 install click
 
 # Download ${TOOLCHAIN_FILE}
 /usr/bin/wget ${WGET_OPTIONS} "${TOOLCHAIN_URL}"
 /usr/bin/rm -r -f /opt/tool-chains
-/usr/bin/7z x -o/opt/ "/opt/${TOOLCHAIN_FILE}"
+/usr/bin/tar mxvfjp "/opt/${TOOLCHAIN_FILE}" -C /opt/
 /usr/bin/sync
 /usr/bin/rm -f "/opt/${TOOLCHAIN_FILE}"
 
@@ -48,7 +51,7 @@ cd libyaul
 change_env_value "YAUL_INSTALL_ROOT" "/opt/tool-chains"
 change_env_value "YAUL_BUILD_ROOT" "/opt/libyaul/build"
 change_env_value "YAUL_BUILD" "build"
-change_env_value "YAUL_RTAGS" "false"
+change_env_value "YAUL_CDB" "1"
 change_env_value "YAUL_OPTION_DEV_CARTRIDGE" "0"
 mv yaul.env "${HOME}/.yaul.env"
 echo >> "${HOME}/.bashrc"
